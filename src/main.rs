23e236f7ece30327284946586ddedd51cb40b0b5
Rafael Regis <rafael.regisp@gmail.com>
2021-09-11 19:27:03 -0300

    Add borg mode.

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,28 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Cli {
+    #[structopt(short, long)]
+    borg_mode: bool,
     message: String,
+}
+
+fn cow_eyes(args: &Cli) -> String {
+    if args.borg_mode {
+        return String::from("==");
+    }
+    return String::from("oo");
 }
 
 fn main() {
     let args = Cli::from_args();
     let horizontal_dialog_line = "-".repeat(args.message.len() + 2);
 
-    let cow = "
+    let cow = format!("
       \\   ^__^
-       \\  (oo)\\_______
+       \\  ({})\\_______
           (__)\\       )\\/\\
               ||----w |
-              ||     ||";
+              ||     ||", cow_eyes(&args));
     
     println!(" {} \n< {} >\n {}{}", horizontal_dialog_line, args.message, horizontal_dialog_line, cow);
 }
