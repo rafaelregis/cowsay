13971f208f42b6d95c019b4909b22fda9977b36d
Rafael Regis <rafael.regisp@gmail.com>
2021-09-11 18:48:12 -0300

    Implement basic functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let message = std::env::args().nth(1).expect("No message found");
+    let horizontal_dialog_line = "-".repeat(message.len() + 2);
+
+    let cow = "
+      \\   ^__^
+       \\  (oo)\\_______
+          (__)\\       )\\/\\
+              ||----w |
+              ||     ||";
+    
+    println!(" {} \n< {} >\n {}{}", horizontal_dialog_line, message, horizontal_dialog_line, cow);
 }
