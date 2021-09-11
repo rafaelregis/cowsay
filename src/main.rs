0ccc8b2c9f39c6c7da058ef6e37699bddd99de36
Rafael Regis <rafael.regisp@gmail.com>
2021-09-11 19:33:17 -0300

    Add all other standard cow eyes.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,47 @@ use structopt::StructOpt;
 struct Cli {
     #[structopt(short, long)]
     borg_mode: bool,
+    #[structopt(short, long)]
+    dead_mode: bool,
+    #[structopt(short, long)]
+    greedy_mode: bool,
+    #[structopt(short, long)]
+    paranoid_mode: bool,
+    #[structopt(short, long)]
+    stoned_mode: bool,
+    #[structopt(short, long)]
+    tired_mode: bool,
+    #[structopt(short, long)]
+    wired_mode: bool,
+    #[structopt(short, long)]
+    youthful_mode: bool,
     message: String,
 }
 
 fn cow_eyes(args: &Cli) -> String {
     if args.borg_mode {
         return String::from("==");
+    }
+    if args.dead_mode {
+        return String::from("XX");
+    }
+    if args.greedy_mode {
+        return String::from("$$");
+    }
+    if args.paranoid_mode {
+        return String::from("@@");
+    }
+    if args.stoned_mode {
+        return String::from("**");
+    }
+    if args.tired_mode {
+        return String::from("--");
+    }
+    if args.wired_mode {
+        return String::from("OO");
+    }
+    if args.youthful_mode {
+        return String::from("..");
     }
     return String::from("oo");
 }
